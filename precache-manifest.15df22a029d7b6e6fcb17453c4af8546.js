0ab7bdd7c26a888f5cee0f9786d2fbce3346f3b9
Tracy Mullen <tracykmullen@gmail.com>
2020-06-10 20:28:55 -0500

    built app

---
+++ precache-manifest.15df22a029d7b6e6fcb17453c4af8546.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2d6a4d2e6a7f7492bbeba9231fac43a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8136569ce7873078fefc",
+    "url": "./static/css/main.34a4279e.chunk.css"
+  },
+  {
+    "revision": "f1ec20e72d0b84b8ef4c",
+    "url": "./static/js/2.7881eba1.chunk.js"
+  },
+  {
+    "revision": "98c036262d3a121e089376ce86c4cff7",
+    "url": "./static/js/2.7881eba1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8136569ce7873078fefc",
+    "url": "./static/js/main.6f02c115.chunk.js"
+  },
+  {
+    "revision": "8c6368854bfd63c46e28",
+    "url": "./static/js/runtime-main.4d3ab03f.js"
+  }
+]);
